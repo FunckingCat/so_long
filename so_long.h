738094fa770096d8d5de273315e349fec3d8aecd
Toad Yamcha <tyamcha@at-g3.msk.21-school.ru>
2021-12-18 14:44:08 +0300

    defines

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: tyamcha <tyamcha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 10:22:06 by tyamcha           #+#    #+#             */
-/*   Updated: 2021/12/18 11:29:03 by tyamcha          ###   ########.fr       */
+/*   Updated: 2021/12/18 14:43:49 by tyamcha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,20 @@
 # include <stdlib.h>
 # include <fcntl.h>
 # include <mlx.h>
+
+#define TILE 64
+#define HERO   "./assets/hero/hero00.xpm"
+#define HERO1  "./assets/hero/hero01.xpm"
+#define HERO2  "./assets/hero/hero02.xpm"
+#define HERO3  "./assets/hero/hero03.xpm"
+#define GOST   "./assets/gost/gost00.xpm"
+#define GOST1  "./assets/gost/gost01.xpm"
+#define GOST2  "./assets/gost/gost02.xpm"
+#define GOST3  "./assets/gost/gost03.xpm"
+#define COLL   "./assets/land/collectible.xpm"
+#define WALL   "./assets/land/wall.xpm"
+#define OUT    "./assets/land/out.xpm"
+#define GROUND "./assets/land/ground.xpm"
 
 typedef struct s_vars {
 	void		*mlx;
