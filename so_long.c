dd020866be4843251c05cfa80f6dbc7488f594c8
Toad Yamcha <tyamcha@at-g3.msk.21-school.ru>
2021-12-18 16:13:03 +0300

    something_works

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: tyamcha <tyamcha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 10:22:02 by tyamcha           #+#    #+#             */
-/*   Updated: 2021/12/18 14:45:26 by tyamcha          ###   ########.fr       */
+/*   Updated: 2021/12/18 16:12:34 by tyamcha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,20 @@ void	error(char *name, char *desc)
 
 int	main(int argc, char **argv)
 {
+	void	*mlx;
+	void	*win;
+	int		a, b;
+
 	t_vars	vars;
 	void	*img;
 
 	if (argc != 2)
 		error("invalid args", "usage: ./so_long map.ber");
 	read_map(&vars, argv[1]);
-	vars.sprite_size = 64;
+
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, vars.map_width * vars.sprite_size, vars.map_height * vars.sprite_size, "Escape");
+	vars.win = mlx_new_window(vars.mlx, vars.map_width * TILE, vars.map_height * TILE, "Hello world!");
+	img = mlx_xpm_file_to_image(vars.mlx, HERO, &a, &b);
+	mlx_put_image_to_window(vars.mlx, vars.win, img, 0, 0);
 	mlx_loop(vars.mlx);
 }
