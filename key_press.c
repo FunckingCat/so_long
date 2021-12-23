faffb3b5243a1e835ac26b8f547ae8e924daff6f
Toad Yamcha <tyamcha@at-g3.msk.21-school.ru>
2021-12-23 12:46:00 +0300

    wasd

--- key_press.c
+++ key_press.c
@@ -6,7 +6,7 @@
 /*   By: tyamcha <tyamcha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 18:01:09 by tyamcha           #+#    #+#             */
-/*   Updated: 2021/12/18 19:29:57 by tyamcha          ###   ########.fr       */
+/*   Updated: 2021/12/23 12:38:18 by tyamcha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 
 int	key_press(int keycode, t_vars *vars)
 {
-	printf("KEY CODE %d\n", keycode);
+	if (keycode == A_KEY)
+		printf("A\n");
+	if (keycode == S_KEY)
+		printf("S\n");
+	if (keycode == D_KEY)
+		printf("D\n");
+	if (keycode == W_KEY)
+		printf("W\n");
 	return (0);
 }
 
