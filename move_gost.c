9eec3ebe3994406c87f29fc07dea71d9b80c8414
Toad Yamcha <tyamcha@at-g3.msk.21-school.ru>
2021-12-25 15:10:30 +0300

    seems_stable

--- move_gost.c
+++ move_gost.c
@@ -6,7 +6,7 @@
 /*   By: tyamcha <tyamcha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/25 12:42:56 by tyamcha           #+#    #+#             */
-/*   Updated: 2021/12/25 13:53:48 by tyamcha          ###   ########.fr       */
+/*   Updated: 2021/12/25 15:09:20 by tyamcha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,33 +22,49 @@ void	move_pos(t_vars *vars, int x, int y, int d1,int d2)
 		vars->map[x][y] = '0';
 		vars->map[x + d1][y + d2] = 'G';
 	}
-	else if (vars->map[x + d1][y + d2] == 'P')
-	{
-		put_image(vars, GROUND, y * TILE, x * TILE);
-		printf("YOU LOST\n");
-		mlx_close();
-	}
 }
 
 void	move(t_vars *vars, size_t x, size_t y, size_t px, size_t py)
 {
 	// printf("-----> %ld %ld\n", x, y);
 	// printf("--X    %ld %ld\n", px, py);
-	// int	rand;
+	int	rand;
+	int	dir;
 
-	// rand = ((vars->steps * x - py) / px + vars->coins_collected * py) % 4;
-	move_pos(vars, x, y, 1, 0);
-	// if (rand == 0) //horizontal
-	// {
-	// 	if (px - x < 0)
-	// 	{
-			
-	// 	}
-	// }
-	// else if (rand == 1 || px - x == 0)
-	// {
-		
-	// }
+	rand = ((vars->steps * x - py) / px + vars->coins_collected * py) % 2;
+	//printf("RAND %d\n", rand);
+	// printf("PX %d PY %d\n", px, py);
+	// printf(" X %d  Y %d\n",  x,  y);
+	if (rand == 1)
+	{
+		dir = px - x;
+		printf("%d - %d = %d\n", py, y, dir);
+		if (dir < 0)
+		{
+			printf("GO UP\n\n");
+			move_pos(vars, x, y, -1, 0);
+		}
+		else if (dir > 0)
+		{
+			printf("GO DOWN\n\n");
+			move_pos(vars, x, y, 1, 0);
+		}
+	}
+	else
+	{
+		dir = py - y;
+		printf("%d - %d = %d\n", py, y, dir);
+		if (dir < 0)
+		{
+			printf("GO LEFT\n\n");
+			move_pos(vars, x, y, 0, -1);
+		}
+		else if (dir > 0)
+		{
+			printf("GO RIGHT\n\n");
+			move_pos(vars, x, y, 0, 1);
+		}
+	}
 }
 
 void	move_gost(t_vars *vars)
@@ -65,7 +81,8 @@ void	move_gost(t_vars *vars)
 			if (vars->map[i][j] == 'G' && vars->bonus)
 			{
 				move(vars, i, j, vars->pl_y, vars->pl_x);
-				break ;
+				j++;
+				i++;
 			}
 			j++;
 		}
